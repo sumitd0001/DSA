2b2826a4ccc84e1a7dc7a9f60960310911c645f7
Sumit Dahiya <sumit.dahiya@ansys.com>
2022-10-04 23:49:58 +0530

    Dijkastra, Prim, Shaortest Path in DAG

--- Graphs/AdjList.cc
+++ Graphs/AdjList.cc
@@ -13,6 +13,18 @@ void addEdge_Directed_Graph (unordered_map<T, unordered_set<T>>& adj, T u, T v) 
     //adj[v].insert(u);
 }
 
+template <typename T1, typename T2>
+void addEdge_undirected_weighted (unordered_map<T1, set < pair<T1, T2> > >& adj, T1 u, T1 v, T2 cost) {
+    adj[u].insert({v, cost});
+    adj[v].insert({u, cost});
+}
+
+template <typename T1, typename T2>
+void addEdge_directed_weighted (unordered_map<T1, set < pair<T1, T2> > >& adj, T1 u, T1 v, T2 cost) {
+    adj[u].insert({v, cost});
+    //adj[v].insert({u, cost});
+}
+
 template <typename T>
 void calculate_indegree (unordered_map<T, unordered_set<T>>& adj, unordered_map <T, int>& indegree) {
     for(auto s : adj) {
@@ -55,6 +67,42 @@ void topological_sorting_BFS(unordered_map<T, unordered_set<T>>& adj) {
             if (indegree[val] == 0) q.push(val);
         }
     }cout<<"\n";
+}
+
+template <typename T>
+bool detect_cycle_DAG_Kahns_Algo (unordered_map<T, unordered_set<T>>& adj) {
+    cout<<"Detec cycle in DAG using Topological Sort, Kahns Algorithm\n";
+    unordered_map<T, int> indegree;
+    calculate_indegree(adj, indegree);
+    cout<<"indegrees for the given graph nodes\n";
+    for (auto val : indegree) {
+        cout<<val.first<<" "<<val.second<<"\n";
+    }
+
+    int cnt = adj.size();
+    queue <T> q;
+
+    int t{0};
+
+    for (auto val : indegree) {
+        if (val.second == 0) {
+            q.push(val.first);
+            t++;
+        }
+    }
+    while (!q.empty()) {
+        T f = q.front();
+        q.pop();
+
+        for (auto val : adj[f]) {
+            indegree[val]--;
+            if (indegree[val] == 0) {
+                q.push(val);
+                t++;
+            }
+        }
+    }
+    return (cnt != t);
 }
 
 template <typename T>
@@ -315,6 +363,143 @@ void print (const unordered_map<T, unordered_set<T>>& adj) {
     }
 }
 
+template <typename T1, typename T2>
+void shortest_path_DIJKASTRAS_ALGO (unordered_map<T1, set<pair<T1, T2>>>& adj, T1 source) {
+    unordered_map<T1, bool> visited;
+    unordered_map<T1, int> distance;
+
+    for (auto node : adj) {
+        visited[node.first] = false;
+        distance[node.first] = INT_MAX;
+    }
+
+    distance[source] = 0;
+
+    queue<T1> q;
+    q.push(source);
+    while (!q.empty()) {
+        T1 front = q.front();
+        q.pop();
+        for (auto node : adj[front]) {
+                if ((distance[front] + node.second ) < distance[node.first]) {
+                    distance[node.first] = distance[front] + node.second;
+                }
+        }
+
+        visited[front] = true;
+
+        int smallest = INT_MAX;
+        for (auto x : distance) {
+            if (!visited[x.first]) {
+                if (x.second < smallest) {
+                    smallest = x.second;
+                    front = x.first;
+                }
+            }
+        }
+        if (!visited[front]) {
+            q.push(front);
+        }
+    }
+
+    for (auto node : distance) {
+        cout<<"Distance of "<<node.first<<" from "<<source<<" is : "<<node.second<<"\n";
+    }
+}
+
+template <typename T1, typename T2>
+void topological_sort_weighted_DFS (unordered_map<T1, set<pair<T1, T2>>>& adj, unordered_map<T1, bool>& visited, stack<T1>& s, T1 source) {
+    visited[source] = true;
+
+    for (auto node : adj[source]) {
+        if (!visited[node.first]) {
+            topological_sort_weighted_DFS(adj, visited, s, node.first);
+        }
+    }
+    s.push(source);
+}
+
+template <typename T1, typename T2>
+void shortest_path_DAG_weighted (unordered_map<T1, set<pair<T1, T2>>>& adj, T1 source) {
+    unordered_map <T1, bool> visited;
+    stack<T1> s;
+    unordered_map<T1, int> dist;
+
+    for (auto node : adj) {
+        visited[node.first] = false;
+        dist[node.first] = INT_MAX;
+    }
+    dist[source] = 0;
+    topological_sort_weighted_DFS(adj, visited, s, source);
+
+    while(!s.empty()) {
+        T1 top = s.top();
+        s.pop();
+
+        for (auto node : adj[top]) {
+            if(dist[top] + node.second < dist[node.first]) {
+                dist[node.first] = dist[top] + node.second;
+            }
+        }
+    }
+
+    for (auto val : dist) {
+        cout<<"Distance of "<<val.first<<" from "<<source<<" : ";
+        if (val.second == INT_MAX) {
+            cout<<"Can't be reached\n";
+        }
+        else {
+            cout<<val.second<<"\n";
+        }
+    }
+}
+
+template <typename T1, typename T2>
+void minimum_Spanning_Tree_PRIMS_ALGO (unordered_map <T1, set<pair<T1, T2>>>& adj, T1 source) {
+    cout<<"Minimum Spanning Tree : ";
+
+    unordered_map<T1, bool> visited;
+    unordered_map <T1, int> dist;
+    
+    for(auto node : adj) {
+        visited[node.first] = false;
+        dist[node.first] = INT_MAX;
+    }
+    dist[source] = 0;
+
+    queue<T1> q;
+    q.push(source);
+
+    while(!q.empty()) {
+        T1 front = q.front();
+        cout<<front<<" ";
+        q.pop();
+
+        for (auto node : adj[front]) {
+            if(!visited[node.first]) {
+                if(dist[front] + node.second < dist[node.first]) {
+                    dist[node.first] = dist[front] + node.second;
+                }
+            }
+        }
+        visited[front] = true;
+        T2 min {INT_MAX};
+        
+        for (auto node : adj) {
+            if (!visited[node.first]) {
+                if (dist[node.first] < min) {
+                    min = dist[node.first];
+                    front = node.first;
+                }
+            }
+        }
+        if (!visited[front]) {
+            q.push(front);
+        }
+    }
+    cout<<"\n";
+}
+
 int main () {
 
     unordered_map<int, unordered_set<int>> adj;
@@ -347,6 +532,55 @@ int main () {
 
     topological_sorting_BFS(adj_directed);
     topological_sort_DFS(adj_directed);
+    bool isCycle = detect_cycle_DAG_Kahns_Algo(adj_directed);
+    cout<<"Cycle Exists : "<<isCycle<<"\n";
+
+    unordered_map<int, unordered_set<int>> adj_directed_cycle;
+    addEdge_Directed_Graph(adj_directed_cycle, 1, 2);
+    addEdge_Directed_Graph(adj_directed_cycle, 1, 3);
+    addEdge_Directed_Graph(adj_directed_cycle, 2, 3);
+    addEdge_Directed_Graph(adj_directed_cycle, 3, 1);
+    addEdge_Directed_Graph(adj_directed_cycle, 3, 4);
+    addEdge_Directed_Graph(adj_directed_cycle, 4, 4);
+    isCycle = detect_cycle_DAG_Kahns_Algo(adj_directed_cycle);
+    cout<<"Cycle Exists : "<<isCycle<<"\n";
+
+    unordered_map<int, set<pair<int, int>>> adj_directed_weighted;
+    addEdge_directed_weighted(adj_directed_weighted, 1, 2, 2);
+    addEdge_directed_weighted(adj_directed_weighted, 1, 5, 1);
+    addEdge_directed_weighted(adj_directed_weighted, 2, 3, 3);
+    addEdge_directed_weighted(adj_directed_weighted, 5, 3, 2);
+    addEdge_directed_weighted(adj_directed_weighted, 5, 6, 4);
+    addEdge_directed_weighted(adj_directed_weighted, 3, 4, 6);
+    addEdge_directed_weighted(adj_directed_weighted, 6, 4, 1);
+    cout<<"Shortest Path in a DAG weighted : \n";
+    for (auto node : adj_directed_weighted) {
+        shortest_path_DAG_weighted(adj_directed_weighted, node.first); 
+    }
+
+    unordered_map<int, set<pair<int, int>>> adj_undirected_weighted;
+    addEdge_undirected_weighted(adj_undirected_weighted, 1, 2, 4);
+    addEdge_undirected_weighted(adj_undirected_weighted, 1, 3, 8);
+    addEdge_undirected_weighted(adj_undirected_weighted, 2, 3, 11);
+    addEdge_undirected_weighted(adj_undirected_weighted, 2, 4, 8);
+    addEdge_undirected_weighted(adj_undirected_weighted, 3, 5, 7);
+    addEdge_undirected_weighted(adj_undirected_weighted, 3, 6, 1);
+    addEdge_undirected_weighted(adj_undirected_weighted, 4, 5, 2);
+    addEdge_undirected_weighted(adj_undirected_weighted, 4, 7, 7);
+    addEdge_undirected_weighted(adj_undirected_weighted, 4, 8, 4);
+    addEdge_undirected_weighted(adj_undirected_weighted, 5, 6, 6);
+    addEdge_undirected_weighted(adj_undirected_weighted, 6, 8, 2);
+    addEdge_undirected_weighted(adj_undirected_weighted, 7, 8, 14);
+    addEdge_undirected_weighted(adj_undirected_weighted, 7, 9, 9);
+    addEdge_undirected_weighted(adj_undirected_weighted, 8, 9, 10);
+
+    for (auto node : adj_undirected_weighted) {
+        shortest_path_DIJKASTRAS_ALGO(adj_undirected_weighted, node.first);
+    }
+
+    for (auto node : adj_undirected_weighted) {
+        minimum_Spanning_Tree_PRIMS_ALGO(adj_undirected_weighted, node.first);
+    }
 
     return 0;
 }
